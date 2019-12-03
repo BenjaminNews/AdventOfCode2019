812ef1e8288023228e6092c945496a8b3d0e5454
BenjaminNews <benjamin.kane@news.co.uk>
2019-12-03 10:59:59 +0000

    clean up day two

--- src/main/java/DayTwo.java
+++ src/main/java/DayTwo.java
@@ -1,10 +1,8 @@
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class DayTwo {
 
-    public int partOne() {
+    public int partOne(String input) {
         int[] valuesInput = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
         for (int i = 0; i < valuesInput.length && valuesInput[i] != 99; i += 4) {
             if(valuesInput[i] == 1) {
@@ -16,11 +14,13 @@ public class DayTwo {
         return valuesInput[0];
     }
 
-    public int partTwo() {
+    public int partTwo(String input) {
         int[] data = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
         for(int verb = 0; verb < 100; verb++) {
             for(int noun = 0; noun < 100; noun++) {
-                int[] temp = calculate(Arrays.copyOf(data, data.length), noun, verb);
+                data[1] = noun;
+                data[2] = verb;
+                int[] temp = calculate(Arrays.copyOf(data, data.length));
                 if(temp[0] == 19690720) {
                     return 100 * noun + verb;
                 }
@@ -29,9 +29,7 @@ public class DayTwo {
         return -1;
     }
 
-    public static int[] calculate(int[] data, int noun, int verb) {
-        data[1] = noun;
-        data[2] = verb;
+    private static int[] calculate(int[] data) {
         int i = 0;
         while (i < data.length) {
             if (data[i] == 1) {
@@ -48,9 +46,9 @@ public class DayTwo {
 
     public static void main(String[] args) {
         DayTwo dayTwo = new DayTwo();
-        System.out.printf("Part one: %d\n", dayTwo.partOne());
+        System.out.printf("Part one: %d\n", dayTwo.partOne(input));
         dayTwo = new DayTwo();
-        System.out.printf("Part two: %d", dayTwo.partTwo());
+        System.out.printf("Part two: %d", dayTwo.partTwo(input));
     }
 
     private static final String input = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,9,1,19,1,9,19,23,1,23,5,27,2,27,10,31,1,6,31,35,1,6,35,39,2,9,39,43,1,6,43,47,1,47,5,51,1,51,13,55,1,55,13,59,1,59,5,63,2,63,6,67,1,5,67,71,1,71,13,75,1,10,75,79,2,79,6,83,2,9,83,87,1,5,87,91,1,91,5,95,2,9,95,99,1,6,99,103,1,9,103,107,2,9,107,111,1,111,6,115,2,9,115,119,1,119,6,123,1,123,9,127,2,127,13,131,1,131,9,135,1,10,135,139,2,139,10,143,1,143,5,147,2,147,6,151,1,151,5,155,1,2,155,159,1,6,159,0,99,2,0,14,0";
--- src/test/java/DayTwoTest.java
+++ src/test/java/DayTwoTest.java
@@ -4,22 +4,10 @@ import org.junit.Test;
 
 public class DayTwoTest {
 
-    private final String inputOne = "1,9,10,3,2,3,11,0,99,30,40,50";
-
     @Test
-    public void givenInputOne_return_3500() {
-        DayTwo dayTwo = new DayTwo(inputOne);
-        assertEquals(3500, dayTwo.partOne());
-        dayTwo = new DayTwo("1,0,0,0,99");
-        dayTwo.partOne();
-
-        dayTwo = new DayTwo("2,3,0,3,99");
-        dayTwo.partOne();
-
-        dayTwo = new DayTwo("2,4,4,5,99,0");
-        dayTwo.partOne();
-
-        dayTwo = new DayTwo("1,1,1,4,99,5,6,0,99");
-        dayTwo.partOne();
+    public void givenInput_return_3500() {
+        DayTwo dayTwo = new DayTwo();
+        String input = "1,9,10,3,2,3,11,0,99,30,40,50";
+        assertEquals(3500, dayTwo.partOne(input));
     }
 }
